1d56c9da108cdd86255d3880d04bbfd1eda1dac9
Zachary E. Graber <zegraber@iu.edu>
2022-04-25 10:28:52 -0400

    spi work so far

--- Project9/P9_SPI/vsrc/spi.sv
+++ Project9/P9_SPI/vsrc/spi.sv
@@ -16,30 +16,71 @@ module spi(
     output logic        done //logical 1 for 1 cycle indicating new data 
 );
     
+    enum {RX_BYTE_1, RX_BYTE_2, TX_BYTE, DONE} state, next_state, prev_state;
+    enum {READ, WRITE} op, next_op;
+    logic [3:0] _counter;
+    
     logic rst_; //local reset
+    logic next_miso;
 
 
     //combine the chip and SPI resets 
     assign rst_ = rst | ss;
 
-    //replace this with actual logic
-    logic fixme; 
-    assign fixme = 'h0;
-
     //tri-state miso when not in use.  
     // keep this line
-    assign miso = ( rst_ ? 'hz : fixme );
+    assign miso = ( rst_ ? 'hz : next_miso );
 
     always_ff @(posedge clk) begin
         if (rst_) begin
-            ; //finish me
-       end else begin
-            ; //finish me
+            state <= RX_BYTE_1;
+            _counter <= 'h7;
+            op <= READ;
+            done <= 1'h0;
+            next_miso <= 1'b0;    
+       end 
+       else begin
+            prev_state <= state;
+            state <= next_state;
+            op <= next_op;
+            case (state)
+                RX_BYTE_1: begin
+                    done <= 1'b0;
+                    din[_counter] <= mosi;
+                    if (_counter == 'h7) begin
+                        if (mosi == 1) next_op <= READ;
+                        else next_op <= WRITE;
+                    end
+                    if (_counter == 'h0) next_state <= DONE;
+                    else _counter <= (_counter - 1);
+                end
+                
+                DONE: begin
+                    done <= 1'b1;
+                    _counter <= 'h7;
+                    case (prev_state)
+                        RX_BYTE_1: begin
+                            next_state <= RX_BYTE_2;
+                        end
+                        
+                        RX_BYTE_2: begin
+                            if (op == READ) begin
+                                next_state <= TX_BYTE;
+                            end
+                            else begin // op is WRITE
+                                next_state <= RX_BYTE_1;
+                            end
+                        end
+                    endcase
+                end
+                
+                RX_BYTE_2: begin
+                    done <= 1'b0;
+                    din[_counter] <= mosi;
+                    if (_counter == 'h0) next_state <= DONE;
+                    else _counter <= (_counter - 1);
+                end
+            endcase
        end
     end
-    
-    always_comb begin
-        ; //finish me
-    end
-
 endmodule
